23c29a7af77d82ad040d3c8300c00bdb601e7c8b
huolongguo10 <67817373+huolongguo10@users.noreply.github.com>
2021-02-15 17:43:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def read_file(filepath):
   with open(filepath) as fp:
-      content=fp.read();
+      content = fp.read();
   return content
 def write_file(filepath,things)
   fo = open(filepath, "w")
@@ -11,4 +11,13 @@ if __name__ = '__main__' :
   passwd = input('password:')
   _targetpasswd=read_file(passwd)
   targetpasswd = _targetpasswd.split(',')
-  
+  c_m_d = ''
+  for i in range targetpasswd :
+    c_m_d += 'sshpass -p '
+    c_m_d += targetpasswd[i]
+    c_m_d += 'ssh'
+    c_m_d += targetusername
+    c_m_d += '@'
+    c_m_d += targetip
+    c_m_d += '\n'
+    
